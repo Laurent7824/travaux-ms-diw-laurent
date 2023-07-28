96a9e4d008a380aad3a52f6c8a45ea763f76afcd
Pruvot Laurent <pruvot.laurent80@orange.fr>
2023-07-28 10:12:08 +0200

    Traitement données Formulaire de Contact

--- Laurent/BACK/SERVEUR/PHP/formulaireGeneral.php
+++ Laurent/BACK/SERVEUR/PHP/formulaireGeneral.php
@@ -11,6 +11,21 @@
 
 <body>
     <?php
+    //Utilisation de 'foreach' pour parcourir les valeur du tableau '$_POST'
+    foreach ($_POST as $key => $value) {
+        //'{$key}' crée des variables dynamiques en utilisant le nom de chaque clé du tableau
+        ${$key} = $value;
+    }
+    //vérifier si la variable $envoyer est déclarée
+    if (isset($envoyer)) {
+        //Création d'un nouveau fichier nommé avec la date et heure de sa création
+        //'a+' Crée un nouveau fichier si le fichier n'existe pas 
+        $fp = fopen((date("Y-m-d-H-i-s") . ".txt"), "a+");
+        //Données enregistrer dans le fichier
+        fputs($fp, $nom . "," . $prenom . "," . $mail . "," . $tel . "," . $demande);
+    }
+    ?>
+    <?php
     require("header.php");
     ?>
     <div class="container">
@@ -18,38 +33,38 @@
         <br>
         <h2 class="scrolling-title">Nous contacter :</h2>
         <br>
-        <form id="myForm">
+        <form id="myForm" method="post" action="">
             <div class="row">
                 <div class="col-md-6">
                     <label for="nom">Nom</label>
-                    <input type="text" class="form-control" id="nom" placeholder="Votre nom">
+                    <input type="text" class="form-control" id="nom" name="nom" placeholder="Votre nom">
                 </div>
                 <div class="col-md-6">
                     <label for="prenom">Prénom</label>
-                    <input type="text" class="form-control" id="prenom" placeholder="Votre prénom">
+                    <input type="text" class="form-control"name="prenom"  id="prenom" placeholder="Votre prénom">
                 </div>
             </div>
             <div class="row mt-3">
                 <div class="col-md-6">
                     <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" placeholder="Votre email">
+                    <input type="email" class="form-control" name="mail"  id="email" placeholder="Votre email">
                     <small id="emailError" class="text-danger d-none">Veuillez saisir une adresse email valide.</small>
                 </div>
                 <div class="col-md-6">
                     <label for="telephone">Téléphone</label>
-                    <input type="tel" class="form-control" id="telephone" placeholder="Votre téléphone">
+                    <input type="tel" class="form-control" name="tel"  id="telephone" placeholder="Votre téléphone">
                     <small id="telephoneError" class="text-danger d-none">Veuillez saisir un numéro de téléphone valide.</small>
                 </div>
             </div>
             <div class="row mt-3">
                 <div class="col">
                     <label for="demande">Votre demande</label>
-                    <textarea class="form-control" id="demande" rows="5" placeholder="Entrez votre demande"></textarea>
+                    <textarea class="form-control" name="$demande"  id="demande" rows="5" placeholder="Entrez votre demande"></textarea>
                 </div>
             </div>
             <div class="row mt-3">
                 <div class="col text-end">
-                    <button type="submit" class="btn btn-success">Envoyer</button>
+                    <button type="submit"name="envoyer"   class="btn btn-success">Envoyer</button>
                 </div>
             </div>
         </form>
--- Laurent/FRONT/DYNAMIQUE/JAVASCRIPT/formulaireGeneral.js
+++ Laurent/FRONT/DYNAMIQUE/JAVASCRIPT/formulaireGeneral.js
@@ -4,32 +4,39 @@ const emailInput = document.getElementById("email"); // Champ d'entrée pour l'a
 const emailError = document.getElementById("emailError"); // Élément pour afficher l'erreur de l'adresse e-mail
 const telephoneInput = document.getElementById("telephone"); // Champ d'entrée pour le numéro de téléphone
 const telephoneError = document.getElementById("telephoneError"); // Élément pour afficher l'erreur du numéro de téléphone
+const inputs = form.querySelectorAll("input, textarea"); // Sélection de tous les champs de saisie et zones de texte du formulaire
+let isValid = true; // Variable pour suivre la validité globale du formulaire
 
 // Ajout d'un écouteur d'événement sur la soumission du formulaire
-form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Empêche l'envoi du formulaire par défaut
-
+form.addEventListener("submit", validateForm);
+ 
     // Vérification de la validité du formulaire
-    if (validateForm()) {
-        alert("Formulaire envoyé avec succès !!!");
+    // if (validateForm()) {
+    //     alert("Formulaire envoyé avec succès !!!");
         // Ici, vous pouvez ajouter le code pour envoyer le formulaire à votre serveur ou autre traitement.
-    }
-});
+    // }
+
 
 // Fonction pour valider le formulaire
-function validateForm() {
-    const inputs = form.querySelectorAll("input, textarea"); // Sélection de tous les champs de saisie et zones de texte du formulaire
-    let isValid = true; // Variable pour suivre la validité globale du formulaire
+function validateForm(e) {
+    e.preventDefault(); // Empêche l'envoi du formulaire par défaut
+  
+        // Parcourir chaque champ de saisie pour vérifier s'il est vide ou non
+        inputs.forEach(input => {
+            if (input.value.trim() === "") { // Si le champ est vide (après suppression des espaces avant et après)
+                input.classList.add("error"); // Ajout de la classe "error" pour appliquer un style d'erreur
+                isValid = false; // Le formulaire n'est pas valide car il y a au moins un champ vide
+            } else {
+                input.classList.remove("error"); // Suppression de la classe "error" si le champ n'est pas vide
+            }
+        });
+    
+    
+    // if (validateForm()) {
+    //     alert("Formulaire envoyé avec succès !!!");
+        // Ici, vous pouvez ajouter le code pour envoyer le formulaire à votre serveur ou autre traitement.
+    // }
 
-    // Parcourir chaque champ de saisie pour vérifier s'il est vide ou non
-    inputs.forEach(input => {
-        if (input.value.trim() === "") { // Si le champ est vide (après suppression des espaces avant et après)
-            input.classList.add("error"); // Ajout de la classe "error" pour appliquer un style d'erreur
-            isValid = false; // Le formulaire n'est pas valide car il y a au moins un champ vide
-        } else {
-            input.classList.remove("error"); // Suppression de la classe "error" si le champ n'est pas vide
-        }
-    });
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Expression régulière pour vérifier le format de l'adresse e-mail
 
@@ -54,6 +61,8 @@ function validateForm() {
         telephoneInput.classList.remove("error"); // Suppression de la classe "error" si le numéro de téléphone est valide
         telephoneError.classList.add("d-none"); // Masquage de l'élément d'erreur lié au numéro de téléphone
     }
+    alert("Formulaire envoyé avec succès !!!");
 
     return isValid; // Renvoie true si le formulaire est valide, sinon false
 }
+
